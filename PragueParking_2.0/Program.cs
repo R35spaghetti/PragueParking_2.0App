ec540bb3be3afd51b01cfa3be62dd621662ffbec
R35spaghetti <trettiofem35@gmail.com>
2022-03-02 15:40:53 +0100

    JSON och få in testdata kvar

--- PragueParking_2.0/Program.cs
+++ PragueParking_2.0/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GhostSheriffsDatabaseAccess;
+
 Console.WriteLine("Hello, World!");
+
+
+
+//TODO skapa testa och se om testdatan åker in - fler metoder
+//TODO JSON för parkeringsplatser, fordonsmodeller, antalet som får sitta i samma plats
+
+VehicleContext context = new();
+context.Database.EnsureCreated();
