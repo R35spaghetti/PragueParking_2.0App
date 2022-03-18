7b4a768bb9d7b873e121fc7640ebb2f3c9cd3799
R35spaghetti <trettiofem35@gmail.com>
2022-03-18 19:43:06 +0100

    Kan skriva ut alla sifferrutor, nu ska man bara försöka byta ut dem också

--- UI/TestShowButtonsVehicleScreen.cs
+++ UI/TestShowButtonsVehicleScreen.cs
@@ -13,8 +13,12 @@ namespace UI
 {
     public partial class TestShowButtonsVehicleScreen : Form
     {
-
-
+        //TODO
+        //1. gör tabellen och döp den, under programmets gång
+        //        //2. gå igenom dina fordonsvärden
+        //        //3. plocka ut alla parkeringsplatser o lägg i en array
+        //        //4. jämför dessa parkeringsplatser med tabellen
+        //        //5. om samma, byt ut
 
         public TestShowButtonsVehicleScreen()
         {
@@ -26,16 +30,14 @@ namespace UI
         {
             ParkingGarageLimitations parkingGarageLimitations = new();
             int parkingSpotsInTheGarage = parkingGarageLimitations.GetOneIntValueFromJsonFile(3);
-            ParkingGarageLogic logic = new();
 
 
             //Antalet platser beroende på maximala antalet parkeringsplatser
             var rows = parkingSpotsInTheGarage / 10;
-            var columns = parkingSpotsInTheGarage/ 10;
-            var car = "";
-            var mc = "";
-            var previousEntryMC = "";
-            var previousEntryCar = "";
+            var columns = parkingSpotsInTheGarage / 10;
+            
+
+
             int currentParkingSpot = 0;
             int counter = 0;
 
@@ -45,7 +47,7 @@ namespace UI
             this.VehicleTableLayoutPanel.RowCount = rows;
 
 
-            //Annars ser det skumt ut
+            //Rensa standardvärden, är skumt utan dessa
             this.VehicleTableLayoutPanel.ColumnStyles.Clear();
             this.VehicleTableLayoutPanel.RowStyles.Clear();
 
@@ -53,11 +55,11 @@ namespace UI
             //Storleken på knapparna i pixlar
             for (int i = 0; i < columns; i++)
             {
-                this.VehicleTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute,100));
+                this.VehicleTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100));
             }
             for (int i = 0; i < rows; i++)
             {
-                this.VehicleTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute,100));
+                this.VehicleTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100));
             }
 
             //Två loopar för att skriva ut vardera knapp, lodrätt och vågrätt
@@ -67,58 +69,111 @@ namespace UI
 
                 for (int j = 0; j < columns; j++)
                 {
-                    //Få ut nummerplåten och  från databasen
-                    car = logic.PresentVehicles(car, currentParkingSpot, "Car");
-                    mc = logic.PresentVehicles(mc, currentParkingSpot, "Motorcycle");
-                    currentParkingSpot+=2;
+                   
+
+                    //string[] numbers = mc.Split('|');
+                    //int parkingSpotNumber = int.Parse(numbers[1]);
+                    //currentParkingSpot += 2;
                     counter++;
 
                     //En ny knapp vid varje ny position
                     var VehicleInfoButton = new Button();
 
+                    //Lägger till siffror, som 1,2,3,4,5 osv
+                    VehicleInfoButton.Text = string.Format("{0}", counter);
+                    //Namnger kontrollerna
+                    VehicleInfoButton.Name = string.Format("{0}",counter);
 
-                    //TODO ta endast ut nummerplåten här, använd siffran för att skippa en knapp
+                    //Lägger till knappen vid rad "x" och kolumn "y" i rutnätet.
+                    this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton, j, i);
+                    VehicleInfoButton.Dock = DockStyle.Fill;
 
-                    //Fyller knappen med nummerplåten från bil
-                    if (mc != "" && mc != previousEntryMC)
+                    //Siffran 12 byts ut mot hej världen
+                    if(VehicleInfoButton.Name == "12")
                     {
-                        VehicleInfoButton.Text = string.Format(mc);
-                        previousEntryMC = mc;
+                        VehicleInfoButton.Text = "Hej världen";
                         this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton);
-                        VehicleInfoButton.Dock = DockStyle.Fill;
-
-
-
                     }
 
-                    //Fyller knappen med nummerplåten från motorcykel
-                    if (car != "" && car != previousEntryCar)
-                    {
-                        VehicleInfoButton.Text = string.Format(car);
-                        previousEntryCar = car;
-                        this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton);
-                        VehicleInfoButton.Dock = DockStyle.Fill;
+                    //För långsam
+             //   ReplaceNumericButtonsWithNumberPlateAndParkingSpot(VehicleInfoButton, parkingSpotsInTheGarage);
 
-                    }
-                    //Lägger till knappen vid rad "x" och kolumn "y" i kolumnen.
-                    //Lägger till resten av knapparna 1, 2, 3, 4 osv
-                    //else
-                    //{
-                    //    //Namnger enligt räknaren
+                }
+            }
+        }
+        //Njaa
+        private void ReplaceNumericButtonsWithNumberPlateAndParkingSpot(Button VehicleInfoButton, int parkingSpotsInTheGarage)
+        {
+            ParkingGarageLogic logic = new();
 
-                    //    VehicleInfoButton.Text = string.Format("{0}", counter);
-                    //    this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton, j, i);
-                    //    VehicleInfoButton.Dock = DockStyle.Fill;
+            var carNumberPlateAndParkingSpot = "";
+            var motorcycleNumberPlateAndParkingSpot = "";
 
-                    //}
+            var previousEntryMC = "";
+            var previousEntryCar = "";
+
+            for (int i = 0; i < parkingSpotsInTheGarage; i++)
+            {
+
+                //Få ut nummerplåten och parkeringsplatsen från databasen
+                carNumberPlateAndParkingSpot = logic.PresentVehicles(carNumberPlateAndParkingSpot, parkingSpotsInTheGarage, "Car");
+                motorcycleNumberPlateAndParkingSpot = logic.PresentVehicles(motorcycleNumberPlateAndParkingSpot, parkingSpotsInTheGarage, "Motorcycle");
+
+                if (carNumberPlateAndParkingSpot != "" && carNumberPlateAndParkingSpot != previousEntryCar)
+                {
+                    previousEntryCar = carNumberPlateAndParkingSpot;
 
 
+                }
+
+                if (motorcycleNumberPlateAndParkingSpot != "" && motorcycleNumberPlateAndParkingSpot != previousEntryMC)
+                {
+                    previousEntryMC = motorcycleNumberPlateAndParkingSpot;
 
 
                 }
             }
         }
 
-    
+
+
+        //TODO ta endast ut nummerplåten här, använd siffran för att skippa en knapp
+
+        ////Fyller knappen med nummerplåten från bil
+        //if (mc != "" && mc != previousEntryMC)
+        //{
+        //    VehicleInfoButton.Text = string.Format(mc, counter);
+        //    VehicleInfoButton.BackColor = Color.Green;
+        //    previousEntryMC = mc;
+        //    this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton, j, i);
+        //    VehicleInfoButton.Dock = DockStyle.Fill;
+
+        //    if (parkingSpotNumber != counter)
+        //    {
+        //        VehicleInfoButton.Text = string.Format("{0}", counter);
+        //        //Namnger kontrollerna
+        //        VehicleInfoButton.Name = string.Format("{0}",counter);
+        //        this.VehicleTableLayoutPanel.Controls.Add(VehicleInfoButton, j, i);
+        //        VehicleInfoButton.Dock = DockStyle.Fill;
+
+        //    }
+        //    if (this.VehicleTableLayoutPanel.Name = parkingSpotNumber)
+        //    {
+        //        //ersätt värden här
+
+
+        //      
+        //    }
+        //}
+
+
+
+
+
+
+
     }
-}
+
+
+                }
+
