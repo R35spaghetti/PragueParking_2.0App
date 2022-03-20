87c2bad69c595f9d36d6d7ed9384c44a7e448720
R35spaghetti <90850331+R35spaghetti@users.noreply.github.com>
2022-03-20 17:23:57 +0100

    Add files via upload
    Istället för zip.

---
+++ UI/bin/Debug/net6.0-windows/ParkingGarageLimitationValues.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+namespace PragueParking_2._0
+{
+
+    public class ParkingGarageLimitationValues
+    {
+        //1
+       [JsonPropertyName("Per hour for Car")]
+        public int PerHourForCar { get; set; }
+
+        //2
+        [JsonPropertyName("Per hour for MC")]
+        public int PerHourForMC { get; set; }
+      
+        //3
+        [JsonPropertyName("Parking spots in the garage")]
+        public int ParkingSpotsInTheGarage { get; set; }
+
+        //4
+        [JsonPropertyName("Vehicle Car")]
+        public string? VehicleTypeCar { get; set; }
+
+        //5
+        [JsonPropertyName("Vehicle Motorcycle")]
+        public string? VehicleTypeMotorcycle { get; set; }
+
+        //6
+        [JsonPropertyName("Vehicle Size Car")]
+        public int VehicleSizeCar { get; set; }
+        
+        //7
+        [JsonPropertyName("Vehicle Size Motorcycle")]
+        public int VehicleSizeMotorcycle { get; set; }
+
+        //8
+        [JsonPropertyName("Parking space size")]
+        public int ParkingSpaceSize { get; set; }
+    }
+
+
+}
+
